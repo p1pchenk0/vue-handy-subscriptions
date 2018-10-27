51a6799207333ad440144465152126c40a4db923
p1pchenk0 <art.pipchenko@gmail.com>
2018-10-27 15:48:31 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-"# vue-handy-subscriptions" 
+# vue-handy-subscriptions
+This plugin is for easier Vue event subsciptions management
+
+## Installation
+```javascript
+import HandySubs from 'vue-handy-subscriptions'
+
+Vue.use(HandySubs)
+```
+
+## Events management
+This package is responsible for automatic events unsubscription when component is being destroyed. No more need to write something like:
+```javascript
+    beforeDestroy() {
+        this.$eventBus.off('event-one', this.methodOne)
+        this.$eventBus.off('event-two', this.methodTwo)
+        this.$eventBus.off('event-three', this.methodThree)
+        this.$eventBus.off('event-four', this.methodFour)
+        // ... and so on
+    }
+```
+Instead of above you should write:
+```javascript
+```
+Yes. Correct. Nothing. Plugin will handle all of this by itself.
+
+### Methods
+Listening to event (example):
+```javascript
+    created() {
+        this.$listenTo('some-event', this.eventCallback)
+    }
+```
+
+Emitting event (example):
+```javascript
+    methods: {
+        fireEvent() {
+            this.$emitEvent('some-event', { test: 'one' })
+        }
+    }
+```
+
+Removing event from events object for all listeners (example):
+```javascript
+    methods: {
+        dontWannaListenAnymore() {
+            this.$eraseEvent('some-event')
+        }
+    }
+```
+
+Unsubsribe from all events manually (example):
+```javascript
+    methods: {
+        leaveMeAlone() {
+            this.$fallSilent() // it is also done automatically
+        }
+    }
+```
+
+By using standart event bus approach with `new Vue()` we create a `Vue` object with lots of unused methods and properties. `vue-handy-subscriptions` creates simple object containing events-related functionality. 
+
---
+++ index.js
@@ -0,0 +1,60 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+exports.default = {
+    install: function install(Vue, options) {
+        Vue.mixin({
+            created: function created() {
+                this._uniqID = Math.random().toString(36).substr(2, 9);
+            },
+            beforeDestroy: function beforeDestroy() {
+                if (this.shouldFallSilent) this.$fallSilent();
+            }
+        });
+
+        Vue.prototype.$idSubs = { _events: {} };
+        var events = Vue.prototype.$idSubs._events;
+        Vue.prototype.$listenTo = function (eventName, cb) {
+            var ID = this._uniqID;
+
+            if (!events[eventName]) {
+                events[eventName] = [];
+            }
+
+            events[eventName].push({
+                subscriberId: ID,
+                callback: cb
+            });
+        };
+
+        Vue.prototype.$emitEvent = function (eventName, options) {
+            var eventsAmount = events[eventName].length;
+
+            if (eventsAmount) {
+                for (var listenerIndex = 0; listenerIndex < eventsAmount; listenerIndex++) {
+                    events[eventName][listenerIndex].callback(options);
+                }
+            }
+        };
+
+        Vue.prototype.$fallSilent = function () {
+            var ID = this._uniqID;
+
+            if (!isEmpty(events)) {
+                for (var event in events) {
+                    for (var listenerIndex = 0; listenerIndex < events[event].length; listenerIndex++) {
+                        if (events[event][listenerIndex].subscriberId === ID) {
+                            events[event].splice(listenerIndex, 1);
+                        }
+                    }
+                }
+            }
+        };
+    }
+};
